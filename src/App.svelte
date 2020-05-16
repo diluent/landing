38491427d90278af19ae5208cbf664b3e764fa77
Dmitry Kotik <d.kotik@corp.mail.ru>
2020-05-17 00:51:05 +0300

    feuer frei

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { onMount } from "svelte";
+  import StartScreen from "./StartScreen.svelte";
+  // import UserSearch from "./UserSearch.svelte";
+
+  // create a variable to store our users
+  let users;
+
+  // run this when the app starts
+  onMount(() => {
+    getGitHubUsers();
+  });
+
+  /**
+   * Grab users from GitHub
+   */
+  function getGitHubUsers() {
+    fetch("https://api.github.com/users")
+      .then(resp => resp.json())
+      .then(data => (users = data));
+  }
+</script>
+
+<style>
+</style>
+
+<main>
+
+  <!-- <UserSearch /> -->
+
+  <!-- {#if users}
+  <ul class="user-list">
+    {#each users as user}
+      <li>
+        <User username={user.login} avatar={user.avatar_url} />
+      </li>
+    {/each}
+  </ul>
+  {/if} -->
+
+  <StartScreen />
+  
+</main>
---
+++ src/StartScreen.svelte
@@ -0,0 +1,41 @@
+<script>
+    const posters = [
+        {
+            title: 'synesthesia',
+            src: 'img/img1.jpg',
+        },
+        {
+            title: 'RTI',
+            src: 'img/img2.jpg',
+        },
+        {
+            title: 'AIWS',
+            src: 'img/img3.jpg',
+        }
+    ];
+</script>
+
+<style>
+    .wrapper {
+        display: flex;
+    }
+
+    .poster {
+        display: flex;
+        justify-content: center;
+        width: 33%;
+    }
+
+    .poster img {
+        width: 90%;
+    }
+</style>
+
+<div class="wrapper">
+    {#each posters as poster}
+        <div class="poster">
+            <!-- <p>{poster.title}</p> -->
+            <img src={poster.src} alt={poster.title} />
+        </div>
+    {/each}
+</div>
